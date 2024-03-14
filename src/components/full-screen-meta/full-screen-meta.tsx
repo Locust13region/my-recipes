c5c5d849e0eef6673a132b9badbd3e8537a55d56
DESKTOP-MF8V5D1\Sergei <m>
2024-03-14 21:44:59 +0300

    PWA

--- src/components/full-screen-meta/full-screen-meta.tsx
+++
@@ -1,8 +0,0 @@
-const FullScreenMeta: React.FC = () => (
-	<meta
-		name="apple-mobile-web-app-capable"
-		content="yes"
-	/>
-);
-
-export default FullScreenMeta;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	// plugins: [react()],
+	plugins: [
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "My recipes",
+				short_name: "MR",
+				description: "Мои рецепты",
+				orientation: "portrait",
+				display: "standalone",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "/img/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/img/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 	server: {
-		host: false,
+		host: true,
 	},
 	// base: "/my-recipes",
 });
